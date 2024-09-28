72fef03e2618cf9e71e3a158b50d3a8e4418fda4
Yaroslav Tokmakov <160162352+sslavatokmakov@users.noreply.github.com>
2024-09-28 13:33:57 +0300

    Revert "Add bookings"

--- src/main/java/ru/practicum/shareit/exception/booking/InvalidBookingPeriodException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception.booking;
-
-public class InvalidBookingPeriodException extends RuntimeException {
-    public InvalidBookingPeriodException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/exception/item/ItemNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception.item;
-
-public class ItemNotFoundException extends RuntimeException {
-    public ItemNotFoundException(String message) {
-        super(message);
-    }
-}
