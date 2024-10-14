661a22b6a5673b0703d06b4f8290b2e3e3f6823d
Yaroslav Tokmakov <ytokmakoff@gmail.com>
2024-10-14 20:25:16 +0300

    sprint 16

--- shareIt-server/src/test/java/ru/practicum/shareitserver/user/service/UserServiceImplTest.java
+++ shareIt-server/src/test/java/ru/practicum/shareitserver/user/service/UserServiceImplTest.java
@@ -7,7 +7,6 @@ import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareitserver.exception.user.EmailAlreadyExistException;
 import ru.practicum.shareitserver.exception.user.UserNotFoundException;
