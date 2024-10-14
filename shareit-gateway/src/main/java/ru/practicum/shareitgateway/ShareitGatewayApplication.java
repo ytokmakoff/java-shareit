53f4d2ae2c35cefb96589cd546419a727c5af2e7
Yaroslav Tokmakov <ytokmakoff@gmail.com>
2024-10-14 11:50:32 +0300

    sprint 16

---
+++ shareit-gateway/src/main/java/ru/practicum/shareitgateway/ShareitGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareitgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareitGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareitGatewayApplication.class, args);
+    }
+
+}
---
+++ shareit-server/src/main/java/ru/practicum/shareitserver/ShareitServerApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareitServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareitServerApplication.class, args);
+    }
+
+}
