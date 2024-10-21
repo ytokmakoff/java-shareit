b336e24de6307766c7509d9b41ef7ece53a796d2
Yaroslav Tokmakov <ytokmakoff@gmail.com>
2024-10-21 13:30:49 +0300

    sprint 16

--- gateway/src/test/java/ru/practicum/shareitgateway/item/ItemClientTest.java
+++ gateway/src/test/java/ru/practicum/shareitgateway/item/ItemClientTest.java
@@ -22,7 +22,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-
 class ItemClientTest {
     @Mock
     private RestTemplateBuilder restTemplateBuilder;
